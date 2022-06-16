ad8299917cc7a98322f6b0b29139f8561b60c2b8
Mathias <77168097+Reigniteh@users.noreply.github.com>
2022-06-16 07:51:10 +0200

    First commit

---
+++ fyreapi/fyreapi.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* FyreAPI PHP implementation with premade classes & functions
+* @author Reigniteh
+* @copyright (c) 2022, Toth Matyas
+* @version 1.0
+* @link https://github.com/Reigniteh/FyreAPI
+* Latest Update: 2022. 06. 16.
+*
+**/
+
+?>
